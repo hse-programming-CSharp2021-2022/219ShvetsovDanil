0f64f0bee5aabaa928fefba8e3faae5d3e8a6b4b
ShvDanil <dishvetsov@edu.hse.ru>
2021-09-21 21:01:45 +0300

    adding class work of 5th seminar

---
+++ 01_module/05_seminar/class_work/Task_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        public static void Sums(uint number, out uint sumEven, out uint sumOdd)
+        {
+            sumEven = 0;
+            sumOdd = 0;
+            while (number > 0)
+            {
+                sumOdd += number % 10;
+                number /= 10;
+                sumEven += number % 10;
+                number /= 10;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            uint number;
+            uint.TryParse(Console.ReadLine(), out number);
+            uint sumEven, sumOdd;
+            Sums(number, out sumEven, out sumOdd);
+            Console.WriteLine($"odd sum = {sumOdd}, even sum = {sumEven}");
+        }
+    }
+}
---
+++ 01_module/05_seminar/class_work/Task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 96;
+            sbyte b = 112;
+            sbyte c = 0;
+            
+            sbyte res1 = (sbyte)(a + b);
+            sbyte res2 = unchecked ((sbyte)(96 + 112));
+            sbyte res3 = (sbyte) (96 + 112 + c);
+            
+            Console.WriteLine(res1);
+        }
+    }
+}
