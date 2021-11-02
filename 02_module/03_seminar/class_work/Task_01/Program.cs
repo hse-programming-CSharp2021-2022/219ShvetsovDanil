70ed7bf0cedeacb65fd26dc02d9523005217fbb4
ShvDanil <dishvetsov@edu.hse.ru>
2021-11-02 19:38:37 +0300

    adding example of class work of 3rd seminar

---
+++ 02_module/03_seminar/class_work/Task_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class ArithmeticProgression
+{
+    public int A0 { get; private set; }
+    public int D { get; private set; }
+    public static int count = 0;
+    public static int count2;
+    static ArithmeticProgression()
+    {
+        Console.WriteLine("static construct");
+    }
+    public ArithmeticProgression(int a0, int d)
+    {
+        A0 = a0; D = d;
+        count++;
+    }
+    public ArithmeticProgression() : this(0, 0) { }
+    public int this[int index]
+    {
+        get
+        {
+            return A0 + D * (index);
+        }
+        private set
+        {
+        }
+    }
+    ~ArithmeticProgression()
+    {
+        Console.WriteLine("Destroy object");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(1);
+        Console.WriteLine(ArithmeticProgression.count2);
+        Console.WriteLine(2);
+        ArithmeticProgression progression = new ArithmeticProgression(2, 10);
+        Console.WriteLine(progression[3]);
+        Console.WriteLine(progression[5]);
+        new ArithmeticProgression(2, 10);
+        new ArithmeticProgression(2, 10);
+        new ArithmeticProgression(2, 10);
+        Console.WriteLine(ArithmeticProgression.count);
+    }
+}
