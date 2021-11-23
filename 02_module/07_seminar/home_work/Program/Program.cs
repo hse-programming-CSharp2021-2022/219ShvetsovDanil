9f81b67e41b2d2f4d66de6fce898f8232af3f5e4
ShvDanil <dishvetsov@edu.hse.ru>
2021-11-23 12:52:10 +0300

    made some changes in hw7

--- 02_module/07_seminar/home_work/Program/Program.cs
+++ 02_module/07_seminar/home_work/Program/Program.cs
@@ -57,103 +57,113 @@ namespace Program
 
         static void Main(string[] args)
         {
-            int n;
             do
             {
-                try
+                int n;
+                do
                 {
-                    Console.Write("Enter amount of creatures: ");
-                    n = int.Parse(Console.ReadLine());
-                    if (n is (<= 0 or > 100))
+                    try
                     {
-                        throw new AmountOfCreaturesException("Incorrect amount of creatures!");
+                        Console.Write("Enter amount of creatures: ");
+                        n = int.Parse(Console.ReadLine());
+                        if (n is (<= 0 or > 100))
+                        {
+                            throw new AmountOfCreaturesException("Incorrect amount of creatures!");
+                        }
+
+                        break;
                     }
-                    break;
-                }
-                catch (AmountOfCreaturesException creaturesException)
-                {
-                    Console.WriteLine(creaturesException.Message + " Try again!");
-                }
-                catch (ArgumentException argumentException)
-                {
-                    Console.WriteLine(argumentException.Message + " Try again!");
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception.Message + " Try again!");
-                }
-            } while (true);
-
-            Console.WriteLine($"Trying to create {n} creatures...");
-
-            Creature[] creatures = new Creature[n];
-            for (var i = 0; i < creatures.Length; i++)
-            {
-                Random random = new Random();
-                double possibility = random.Next(0, 11);
-                if (possibility <= 2)
-                {
-                    do
+                    catch (AmountOfCreaturesException creaturesException)
                     {
-                        try
-                        {
-                            creatures[i] = new Creature(CreateName(), CreateSpeed());
-                            break;
-                        }
-                        catch (IncorrectCreatureNameException incorrectCreatureNameException)
-                        {
-                            Console.WriteLine(incorrectCreatureNameException.Message + " Try again!");
-                        }
-                        catch (IncorrectCreatureSpeedException incorrectCreatureSpeedException)
-                        {
-                            Console.WriteLine(incorrectCreatureSpeedException.Message + " Try again!");
-                        }
-                    } while (true);
-                }
-                else if (possibility is (> 2 and <= 6))
-                {
-                    do
+                        Console.WriteLine(creaturesException.Message + " Try again!");
+                    }
+                    catch (ArgumentException argumentException)
                     {
-                        try
-                        {
-                            creatures[i] = new Dwarf(CreateName(), CreateSpeed(), CreateStrength());
-                            break;
-                        }
-                        catch (IncorrectCreatureNameException incorrectCreatureNameException)
-                        {
-                            Console.WriteLine(incorrectCreatureNameException.Message + " Try again!");
-                        }
-                        catch (IncorrectCreatureSpeedException incorrectCreatureSpeedException)
-                        {
-                            Console.WriteLine(incorrectCreatureSpeedException.Message + " Try again!");
-                        }
-                    } while (true);
-                }
-                else
-                {
-                    do
+                        Console.WriteLine(argumentException.Message + " Try again!");
+                    }
+                    catch (Exception exception)
                     {
-                        try
-                        {
-                            creatures[i] = new Elf(CreateName(), CreateSpeed());
-                            break;
-                        }
-                        catch (IncorrectCreatureNameException incorrectCreatureNameException)
-                        {
-                            Console.WriteLine(incorrectCreatureNameException.Message + " Try again!");
-                        }
-                        catch (IncorrectCreatureSpeedException incorrectCreatureSpeedException)
-                        {
-                            Console.WriteLine(incorrectCreatureSpeedException.Message + " Try again!");
-                        }
-                    } while (true);
-                }
-            }
+                        Console.WriteLine(exception.Message + " Try again!");
+                    }
+                } while (true);
 
-            Console.WriteLine("\nAt least creatures were created!!! Let's see them!\n");
+                Console.WriteLine($"Trying to create {n} creatures...");
+
+                Creature[] creatures = new Creature[n];
+                for (var i = 0; i < creatures.Length; i++)
+                {
+                    Random random = new Random();
+                    double possibility = random.Next(0, 11);
+                    if (possibility <= 2)
+                    {
+                        do
+                        {
+                            try
+                            {
+                                creatures[i] = new Creature(CreateName(), CreateSpeed());
+                                break;
+                            }
+                            catch (IncorrectCreatureNameException incorrectCreatureNameException)
+                            {
+                                Console.WriteLine(incorrectCreatureNameException.Message + " Try again!");
+                            }
+                            catch (IncorrectCreatureSpeedException incorrectCreatureSpeedException)
+                            {
+                                Console.WriteLine(incorrectCreatureSpeedException.Message + " Try again!");
+                            }
+                        } while (true);
+                    }
+                    else if (possibility is (> 2 and <= 6))
+                    {
+                        do
+                        {
+                            try
+                            {
+                                creatures[i] = new Dwarf(CreateName(), CreateSpeed(), CreateStrength());
+                                break;
+                            }
+                            catch (IncorrectCreatureNameException incorrectCreatureNameException)
+                            {
+                                Console.WriteLine(incorrectCreatureNameException.Message + " Try again!");
+                            }
+                            catch (IncorrectCreatureSpeedException incorrectCreatureSpeedException)
+                            {
+                                Console.WriteLine(incorrectCreatureSpeedException.Message + " Try again!");
+                            }
+                        } while (true);
+                    }
+                    else
+                    {
+                        do
+                        {
+                            try
+                            {
+                                creatures[i] = new Elf(CreateName(), CreateSpeed());
+                                break;
+                            }
+                            catch (IncorrectCreatureNameException incorrectCreatureNameException)
+                            {
+                                Console.WriteLine(incorrectCreatureNameException.Message + " Try again!");
+                            }
+                            catch (IncorrectCreatureSpeedException incorrectCreatureSpeedException)
+                            {
+                                Console.WriteLine(incorrectCreatureSpeedException.Message + " Try again!");
+                            }
+                        } while (true);
+                    }
+                }
+
+                Console.WriteLine("\nAt least creatures were created!!! Let's see them!\n");
+
+                PrintCreatureInfo(creatures);
+                PrintNewStaffMessage(creatures);
+
+                Console.WriteLine();
+                Console.WriteLine("Press Backspace to exit the program!");
+            } while (Console.ReadKey().Key != ConsoleKey.Backspace);
             
-            PrintCreatureInfo(creatures);
-            PrintNewStaffMessage(creatures);
+            Console.WriteLine();
+            Console.WriteLine("Good luck!");
         }
     }
 }
