575789f8d656eaa3222bdf99a1e1b38b2480d259
ShvDanil <dishvetsov@edu.hse.ru>
2021-12-07 13:19:36 +0300

    adding class work of 11 and 12 sem

---
+++ 02_module/11and12seminar/class_work/Task_03/Task_03/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBlock tb = new();
+            StackPanel sp = new();
+            Slider slider = new();
+
+            slider.Minimum = -10;
+            slider.Maximum = 10;
+            slider.Orientation = Orientation.Horizontal;
+            slider.Margin = new Thickness(10);
+            slider.ValueChanged += (sender, e) => tb.Text = slider.Value.ToString("F2");
+
+            sp.Children.Add(slider);
+            tb.FontSize = 20;
+            tb.Background = Brushes.LightGray;
+            tb.Width = 85;
+            HorizontalAlignment = HorizontalAlignment.Center;
+            tb.Text = "0";
+            sp.Children.Add(tb);
+            this.Content = sp;
+        }
+    }
+}
---
+++ 02_module/11and12seminar/class_work/class_work/Task_02.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace class_work
+{
+    /// <summary>
+    /// Логика взаимодействия для Task_02.xaml
+    /// </summary>
+    public partial class Task_02 : Window
+    {
+        public Task_02()
+        {
+            InitializeComponent();
+        }
+
+        private void set_Click(object sender, RoutedEventArgs e)
+        {
+            NameBlock.Text.Replace("Username", $"{textBox.Text}");
+            GreetingBlock.Text.Replace("Username", $"{textBox.Text}");
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            NameBlock.Text.Replace($"{textBox.Text}", "Username");
+            GreetingBlock.Text.Replace($"{textBox.Text}", "Username");
+        }
+    }
+}
