57d63be78a8eb6e337884b512e616e71b9ae58cb
ShvDanil <dishvetsov@edu.hse.ru>
2022-01-18 21:24:38 +0300

    adding class work of 3rd sem

---
+++ 03_module/03_seminar/class_work/Task_01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Task_01
+{
+    delegate void SquareSizeChanged(double a, double x1, double y1, double x2, double y2);
+
+    class Square
+    {
+        public event SquareSizeChanged OnSizeChanged;
+
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+    }
+    
+    class Program
+    {
+        private static void SquareConsoleInfo(double a, double x1, double y1, double x2, double y2) => Console.WriteLine($"Square size changed! New length of side: {a:F2}\n" +
+                                                                             $"Left upper corner coord: ({x1}, {y1})\n" +
+                                                                             $"Right lower corner coord: ({x2}, {y2})\n");
+        
+        static void Main(string[] args)
+        {
+            Square s = new();
+            s.OnSizeChanged += SquareConsoleInfo;
+
+            while (true)
+            {
+                Console.WriteLine("Enter 'coords' to input coordinates and 'exit' to quit.");
+                switch (Console.ReadLine())
+                {
+                    case "coords":
+                        Console.WriteLine("Enter following (x, y) coordinates of right lower corner:");
+                        string[] input = Console.ReadLine()?.Split();
+                        var x1 = double.Parse(input[0]);
+                        var y1 = double.Parse(input[1]);
+
+                        Console.Write("Enter length of side: ");
+                        double a = int.Parse(Console.ReadLine());
+                        
+                        s.X1 = x1;
+                        s.Y1 = y1;
+                        s.X2 = x1 - a;
+                        s.Y2 = y1 + a;
+                        
+                        SquareConsoleInfo(a, s.X1, s.Y1, s.X2, s.Y2);
+                        continue;
+                    case "exit":
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect command! Try again!");
+                        continue;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ 03_module/03_seminar/class_work/Task_01_2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Task_01_2
+{
+    delegate void SquareSizeChanged(double a);
+
+    class Square
+    {
+        public event SquareSizeChanged OnSizeChanged;
+
+        private double _x1;
+        private double _y1;
+        private double _x2;
+        private double _y2;
+
+        public Square(double x1, double y1, double x2, double y2)
+        {
+            _x1 = x1;
+            _y1 = y1;
+            _x2 = x2;
+            _y2 = y2;
+        }
+        
+        public double X1 
+        {
+            get => _x1;
+            set
+            {
+                _x1 = value;
+                OnSizeChanged?.Invoke(Math.Abs(_x1 - _x2));
+            }
+        }
+
+        public double Y1
+        {
+            get => _y1;
+            set
+            {
+                _y1 = value;
+                OnSizeChanged?.Invoke(Math.Abs(_y1 - _y2));
+            }
+        }
+
+        public double X2
+        {
+            get => _x2;
+            set
+            {
+                _x2 = value;
+                OnSizeChanged?.Invoke(Math.Abs(_x1 - _x2));
+            }
+        }
+
+        public double Y2
+        {
+            get => _y2;
+            set
+            {
+                _y2 = value;
+                OnSizeChanged?.Invoke(Math.Abs(_y1 - _y2));
+            }
+        }
+    }
+    
+    class Program
+    {
+        private static void SquareConsoleInfo(double a) => Console.WriteLine($"{a:F2}");
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter following (x, y) coordinates of right lower corner:");
+            string[] input = Console.ReadLine()?.Split();
+            var x1 = double.Parse(input[0]);
+            var y1 = double.Parse(input[1]);
+            
+            Console.WriteLine("Enter following (x, y) coordinates of left upper corner:");
+            string[] input1 = Console.ReadLine()?.Split();
+            var x2 = double.Parse(input[0]);
+            var y2 = double.Parse(input[1]);
+            
+            Square s = new(x1, y1, x2, y2);
+            s.OnSizeChanged += SquareConsoleInfo;
+
+            while (true)
+            {
+                Console.WriteLine("Enter side length:");
+                var x = double.Parse(Console.ReadLine());
+                var y = double.Parse(Console.ReadLine());
+                s.X2 = x2;
+                s.Y2 = y2;
+                if (Console.ReadLine() == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
