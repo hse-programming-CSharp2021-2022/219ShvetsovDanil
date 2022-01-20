626adaf6c718e0f922d053bef5877343875b813b
ShvDanil <dishvetsov@edu.hse.ru>
2022-01-20 16:21:32 +0300

    adding hw3

---
+++ 03_module/03_seminar/home_work/Task_01/Dot.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Task_01
+{
+    public delegate void CoordChanged(Dot dot);
+    
+    public class Dot
+    {
+        private readonly Random _random = new();
+        
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Dot(double x, double y) => (X, Y) = (x, y);
+
+        public event CoordChanged OnCoordChanged;
+
+        public void DotFlow()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                X = _random.Next(-10, 11);
+                Y = _random.Next(-10, 11);
+
+                if (X < 0 && Y < 0)
+                {
+                    OnCoordChanged?.Invoke(this);
+                }
+            }
+        }
+    }
+}
---
+++ 03_module/03_seminar/home_work/Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        private static void PrintInfo(Dot A) => Console.WriteLine($"X: {A.X}\nY: {A.Y}\n");
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter (x, y) coordinates: ");
+            string[] input = Console.ReadLine()?.Split();
+            var (x, y) = (double.Parse(input[0]), double.Parse(input[1]));
+
+            Dot D = new(x, y);
+            D.OnCoordChanged += PrintInfo;
+            D.DotFlow();
+        }
+    }
+}
