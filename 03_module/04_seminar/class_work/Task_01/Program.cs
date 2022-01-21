bc935aadbe9887ff323456cb2a33c66a502cdb2c
ShvDanil <dishvetsov@edu.hse.ru>
2022-01-21 16:07:15 +0300

    adding cw4

---
+++ 03_module/04_seminar/class_work/Task_01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task_01
+{
+    class AccountEventArgs
+    {
+        public string Str { get; }
+        public AccountEventArgs(string s) => Str = s;
+    }
+    //public delegate void AccountHandler(string s);
+    class Account
+    {
+        public event EventHandler<AccountEventArgs> Notify;
+        public int Sum { get; set; }
+        public Account(int sum) => Sum = sum;
+        public void Put(int sum)
+        {
+            Sum += sum;
+            Notify?.Invoke(this, new AccountEventArgs("Put" + sum));
+        }
+        public void Take(int sum)
+        {
+            Sum -= sum;
+            Notify?.Invoke(this, new AccountEventArgs("Take" + sum));
+        }
+    }
+    class Program
+    {
+        public void Print(object sender, AccountEventArgs s)
+        {
+            Console.WriteLine(s.Str);
+        }
+        public static void Print2(object sender, AccountEventArgs s)
+        {
+            Console.WriteLine(s.Str);
+        }
+        static void Main(string[] args)
+        {
+            Account account = new Account(1000);
+            account.Notify += (new Program()).Print;
+            account.Notify += Print2;
+            account.Take(100);
+            Console.WriteLine(account.Sum);
+            account.Take(100);
+            Console.WriteLine(account.Sum);
+            account.Put(100);
+            Console.WriteLine(account.Sum);
+        }
+    }
+}
---
+++ 03_module/04_seminar/class_work/Task_03/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task_03
+{
+    public delegate void NewStringValue(string s);
+    
+    public class UIStringEventArgs: EventArgs
+    {
+        public string Str { get; private set; }
+        public UIStringEventArgs(string s) => Str = s;
+    }
+    
+    public class UIString
+    {
+        public void NewStringValueHappenedHandler(object sender, UIStringEventArgs e) => str = e.Str;
+
+        string str = "Default text";
+        public string Str { get { return str; } private set { str = value; } }
+    }
+    
+    class ConsoleUI
+    {
+        public event EventHandler<UIStringEventArgs> NewStringValueHappened;
+        
+        UIString s = new UIString(); // специальная строка
+        public UIString S { get { return s; } set { s = value; } }
+        public void GetStringFromUI()
+        {
+            Console.Write("Введите новое значение строки: ");
+            string str = Console.ReadLine();
+            NewStringValueHappened?.Invoke(this, new(str));
+            RefreshUI();
+        }
+        public void CreateUI()
+        {
+            NewStringValueHappened += s.NewStringValueHappenedHandler;
+            RefreshUI();
+        }
+        public void RefreshUI()
+        {      // обновление строки
+            Console.Clear();
+            Console.WriteLine("Текст строки: " + s.Str);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUI c = new ConsoleUI();
+            c.CreateUI(); // запускаем выполнение объекта класса ConsoleUI
+            do
+            {
+                c.GetStringFromUI();  // изменяем значение строки
+                Console.WriteLine("Чтобы закончить эксперименты, нажмите ESC...");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
