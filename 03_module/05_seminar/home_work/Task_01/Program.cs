0c65ff8a0e7f098d394aeaf207d8be3dd0132616
ShvDanil <dishvetsov@edu.hse.ru>
2022-01-26 08:52:40 +0300

    adding 5hw

---
+++ 03_module/05_seminar/home_work/Task_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task_01
+{
+    public delegate double Calculate(double x);
+    
+    internal interface IFunction
+    {
+        double Function(double x);
+    }
+
+    class F : IFunction
+    {
+        private readonly Calculate _calculate;
+        
+        public F(Calculate calculate)
+        {
+            _calculate = calculate;
+        }
+
+        public double Function(double x) => _calculate(x);
+    }
+
+    class G
+    {
+        private F _f1, _f2;
+        
+        public G(F f1, F f2)
+        {
+            _f1 = f1;
+            _f2 = f2;
+        }
+
+        public double GF(double x0) => _f1.Function(_f2.Function(x0));
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F f1 = new(x => Math.Pow(x, 2) - 4);
+            F f2 = new(x => Math.Sin(x));
+
+            G g = new(f1, f2);
+
+            for (double i = 0; i <= Math.PI; i += Math.PI / 16)
+            {
+                Console.WriteLine($"G({i:F4}) — {g.GF(i):F4}");
+            }
+        }
+    }
+}
