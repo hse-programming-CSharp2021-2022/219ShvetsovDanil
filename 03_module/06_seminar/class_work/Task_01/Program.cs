64f75fd932e3ecb2a17c85ff11455579265fa340
ShvDanil <dishvetsov@edu.hse.ru>
2022-01-28 17:11:26 +0300

    adding 6th cw

---
+++ 03_module/06_seminar/class_work/Task_01/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public static double GetDistance(Point a1, Point b1) => Math.Sqrt((a1.X * a1.X - b1.X * b1.X) + (a1.Y * a1.Y - b1.Y * b1.Y));
+    }
+
+    class TriangleComp
+    {
+        private Point[] _points;
+
+        private double[] _sides;
+
+        public TriangleComp(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            _points = new Point[] { new(x1, y1), new(x2, y2), new(x3, y3)};
+            _sides = new[] { 
+                Point.GetDistance(_points[0], _points[1]), 
+                Point.GetDistance(_points[0], _points[2]),
+                Point.GetDistance(_points[1], _points[2]) 
+            };
+        }
+        
+        public TriangleComp(Point a, Point b, Point c)
+        {
+            _points = new[] { a, b, c };
+            _sides = new[] {
+                Point.GetDistance(_points[0], _points[1]),
+                Point.GetDistance(_points[0], _points[2]), 
+                Point.GetDistance(_points[1], _points[2])
+            };
+        }
+
+        public double Square
+        {
+            get
+            {
+                double p = (_sides[0] + _sides[1] + _sides[2]) / 2;
+                return Math.Sqrt(p * (p - _sides[0]) * (p - _sides[1]) * (p - _sides[2]));
+            }
+        }
+        
+        public bool IsPointInsideTriangle(Point a)
+        {
+            var newTriangle = new TriangleComp(_points[0].X, _points[0].Y, _points[1].X, _points[1].Y, a.X, a.Y);
+            return Square > newTriangle.Square; 
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = new Point[3];
+            for (var i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter {i + 1} point coords in (x, y) format: ");
+                var input = Console.ReadLine()?.Split();
+                points[i] = new Point(double.Parse(input[0]), double.Parse(input[1]));
+            }
+            var triangleComp = new TriangleComp(points[0], points[1], points[2]);
+            
+            Console.WriteLine(triangleComp.Square);
+            Console.WriteLine(triangleComp.IsPointInsideTriangle(new Point(4, 2)));
+        }
+    }
+}
---
+++ 03_module/06_seminar/class_work/Task_02/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task_02
+{
+    abstract class Animal : IComparable<Animal>
+    {
+        protected uint Age { get; init; }
+        
+        public int CompareTo(Animal anotherAnimal)
+        {
+            if (Age < anotherAnimal.Age) 
+                return 1;
+            if (Age > anotherAnimal.Age) 
+                return -1;
+            return 0;
+        }
+        
+        public override string ToString()
+        {
+            return $"Animal: {GetType().Name}; Age: {Age}";
+        }
+    }
+
+    interface IRunnable
+    {
+        void Run() => Console.WriteLine($"{this} is running!");
+    }
+
+    interface IJumpable
+    {
+        void Jump() => Console.WriteLine($"{this} is jumping!");
+    }
+    
+    sealed class Cockroach : Animal, IRunnable
+    {
+        public uint Speed { get; }
+
+        public Cockroach(uint age, uint speed)
+        {
+            Age = age;
+            Speed = speed;
+        }
+        
+        public override string ToString()
+        {
+            return $"{base.ToString()}; Speed: {Speed}";
+        }
+    }
+
+    class CockroachComparer : IComparer<Cockroach>
+    {
+        public int Compare(Cockroach cockroach1, Cockroach cockroach2)
+        {
+            if (cockroach1?.Speed < cockroach2?.Speed)
+                return 1;
+            if (cockroach1?.Speed > cockroach2?.Speed)
+                return -1;
+            return 0;
+        }
+    }
+    
+    sealed class Kangaroo : Animal, IJumpable
+    {
+        public uint JumpLength { get; }
+
+        public Kangaroo(uint age, uint jumpLength)
+        {
+            Age = age;
+            JumpLength = jumpLength;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}; Jump Length: {JumpLength}";
+        }
+    }
+
+    class KangarooComparer : IComparer<Kangaroo>
+    {
+        public int Compare(Kangaroo kangaroo1, Kangaroo kangaroo2)
+        {
+            if (kangaroo1?.JumpLength < kangaroo2?.JumpLength)
+                return 1;
+            if (kangaroo1?.JumpLength > kangaroo2?.JumpLength)
+                return -1;
+            return 0;
+        }
+    }
+    
+    sealed class Cheetah : Animal, IRunnable, IJumpable
+    {
+        public uint Speed { get; }
+        public uint JumpLength { get; }
+
+        public Cheetah(uint age, uint speed, uint jumpLength)
+        {
+            Age = age;
+            Speed = speed;
+            JumpLength = jumpLength;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}; Speed: {Speed}; Jump Length: {JumpLength}";
+        }
+    }
+
+    class Program
+    {
+        private static Random _random = new();
+
+        static void Main(string[] args)
+        {
+            int animalsAmount = _random.Next(10, 20);
+            Console.WriteLine(animalsAmount);
+            var animals = new Animal[animalsAmount];
+
+            for (var count = 0; count < animalsAmount; count += 3)
+            {
+                if (count == animalsAmount) break;
+                animals[count] = new Cockroach(
+                    (uint) _random.Next(2, 10), 
+                    (uint) _random.Next(5, 11)
+                    );
+                
+                if (count == animalsAmount) break;
+                animals[count + 1] = new Kangaroo(
+                    (uint) _random.Next(2, 10), 
+                    (uint) _random.Next(2, 6)
+                    );
+                
+                if (count == animalsAmount) break;
+                animals[count + 2] = new Cheetah(
+                    (uint) _random.Next(2, 10), 
+                    (uint) _random.Next(5, 11),
+                    (uint) _random.Next(2, 5)
+                    );
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("— — — — — — — — — — — — — — — —");
+            
+            Array.Sort(animals);
+            
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("— — — — — — — — — — — — — — — —");
+            
+            List<Cockroach> cockroaches = new();
+            foreach (var animal in animals)
+            {
+                if (animal is Cockroach)
+                    cockroaches.Add(animal as Cockroach);
+            }
+            cockroaches.Sort(new CockroachComparer());
+
+            foreach (var cockroach in cockroaches)
+            {
+                Console.WriteLine(cockroach);
+            }
+            Console.WriteLine("— — — — — — — — — — — — — — — —");
+
+            List<Kangaroo> kangaroos = new();
+            foreach (var animal in animals)
+            {
+                if (animal is Kangaroo)
+                    kangaroos.Add(animal as Kangaroo);
+            }
+            kangaroos.Sort(new KangarooComparer());
+
+            foreach (var kangaroo in kangaroos)
+            {
+                Console.WriteLine(kangaroo);
+            }
+        }
+    }
+}
