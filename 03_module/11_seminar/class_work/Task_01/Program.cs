de9aabd5e4e654fa4148a58e7bedffb95b07a78f
ShvDanil <dishvetsov@edu.hse.ru>
2022-02-18 20:51:32 +0300

    adding 11cw

---
+++ 03_module/11_seminar/class_work/Task_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fileStream = new FileStream("task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                if (fileStream.Length == 0)
+                {
+                    var line = "A";
+                    var array = Encoding.Default.GetBytes(line);
+                    
+                    fileStream.Write(array, 0, array.Length);
+                }
+                else
+                {
+                    var array = new byte[fileStream.Length];
+                    fileStream.Read(array, 0, array.Length);
+                    var str = Encoding.Default.GetString(array);
+                    Console.WriteLine(str);
+                    if (str[^1] == 'C')
+                    {
+                        return;
+                    }
+                    var newString = ((char)(str[^1] + 1)).ToString();
+                    var array2 = Encoding.Default.GetBytes(newString);
+                    fileStream.Position = array.Length;
+                    fileStream.Write(array2, 0, 1);
+                }
+            }
+        }
+    }
+}
---
+++ 03_module/11_seminar/class_work/Task_02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Task_02
+{
+    class Program
+    {
+        private static readonly Random Random = new();
+        
+        static void Main(string[] args)
+        {
+            using (var sw = new StreamWriter(File.Open("task_2.txt", FileMode.Create, FileAccess.Write)))
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    sw.Write((char) Random.Next(1, 101));
+                }
+            }
+
+            int[] array = new int[10];
+            using (var sr = new StreamReader(File.Open("task_2.txt", FileMode.Open, FileAccess.Read)))
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    array[i] = sr.Read();
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+                
+                int.TryParse(Console.ReadLine(), out var n);
+                var min = 1000;
+                var index = 0;
+                
+                for (var i = 0; i < 10; i++)
+                {
+                    if (Math.Abs(n - array[i]) < min)
+                    {
+                        min = Math.Abs(n - array[i]);
+                        index = i;
+                    }
+                }
+                array[index] = n;
+            }
+
+            using (var sw = new StreamWriter(File.Open("task_2.txt", FileMode.Open, FileAccess.Write)))
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    sw.Write((char) array[i]);
+                }
+            }
+            
+            using (var sr = new StreamReader(File.Open("task_2.txt", FileMode.Open, FileAccess.Read)))
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    array[i] = sr.Read();
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
