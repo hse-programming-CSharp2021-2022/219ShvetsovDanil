a50e33e1605e0b10a754acbc24b4363c476f9447
ShvDanil <dishvetsov@edu.hse.ru>
2022-03-12 17:16:09 +0300

    adding HW17

---
+++ 03_module/17_seminar/class_work/Task_02/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Task_02.Models;
+
+namespace Task_02.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>(new[] {
+            new Product() { Id = 1, Name = "Notebook", Price = 100000 },
+            new Product() { Id = 2, Name = "Car", Price = 2000000 },
+            new Product() { Id = 3, Name = "Apple", Price = 30 },
+        });
+        
+        [HttpGet]
+        public IEnumerable<Product> Get() => products;
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = products.SingleOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+        
+        // Done till the end in following HW.
+    }
+}
